4466d593ad4717c1189927afed59678e1de59e94
mitrahe <heydarimitra98@gmail.com>
2023-08-21 16:01:36 +0000

    Add professor position as new field and validators.

--- src/main/java/org/example/entity/UserInfo.java
+++ src/main/java/org/example/entity/UserInfo.java
@@ -1,26 +1,51 @@
 package org.example.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.example.base.entity.BaseEntity;
 import org.example.entity.enumeration.UserRoleEnum;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 @Entity
 @Table(name = "users_info")
 public class UserInfo extends BaseEntity<Long> {
-    private String name;
-    private String surName;
 
+    public UserInfo(String name, String surname, UserRoleEnum role, String professorPosition, UserCredential userCredential) {
+        this.name = name;
+        this.surname = surname;
+        this.role = role;
+        this.professorPosition = professorPosition;
+        this.userCredential = userCredential;
+    }
+
+    @NotNull(message = "cannot be null")
+    private String name;
+
+    @NotNull(message = "cannot be null")
+    private String surname;
+
+    @NotNull(message = "cannot be null")
     @Enumerated(EnumType.STRING)
     private UserRoleEnum role;
 
-    @OneToOne
+    private String professorPosition;
+
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_credential_id")
     private UserCredential userCredential;
 
-    @OneToMany(mappedBy = "teacherInfo")
+    @OneToMany(mappedBy = "professorInfo")
     private List<AvailableCourse> taughtCourses = new ArrayList<>();
 
     @OneToMany(mappedBy = "studentInfo")
