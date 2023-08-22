66b41134b9b18510289fd0375f8dc00dcb7495ca
mitrahe <heydarimitra98@gmail.com>
2023-08-22 19:43:59 +0000

    Add validators and semester field.

--- src/main/java/org/example/entity/SelectedCourse.java
+++ src/main/java/org/example/entity/SelectedCourse.java
@@ -1,14 +1,19 @@
 package org.example.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
 import org.example.base.entity.BaseEntity;
+
+@Getter
+@Setter
 
 @Entity
 @Table(name = "selected_courses")
 public class SelectedCourse extends BaseEntity<Long> {
+
     @ManyToOne
     @JoinColumn(name = "course_id")
     private AvailableCourse course;
@@ -17,6 +22,13 @@ public class SelectedCourse extends BaseEntity<Long> {
     @JoinColumn(name = "student_info_id")
     private UserInfo studentInfo;
 
+    @Min(value = 0, message = "Grade should not be less than 0")
+    @Max(value = 20, message = "Age should not be greater than 20")
     private double grade;
+
     private boolean passed;
+
+    @Min(value = 1, message = "Semester cannot be zero or negative")
+    @Column (name = "student_semester")
+    private int studentSemester;
 }
