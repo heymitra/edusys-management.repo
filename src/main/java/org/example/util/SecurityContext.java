a6437ce66c399cac07a1463e82b4c457e244b01b
mitrahe <heydarimitra98@gmail.com>
2023-08-24 03:22:01 +0000

    Add security context.

---
+++ src/main/java/org/example/util/SecurityContext.java
@@ -0,0 +1,9 @@
+package org.example.util;
+
+import org.example.entity.UserInfo;
+
+public class SecurityContext {
+    public static String username;
+    public static UserInfo userInfo;
+//    public static String currentSemester;
+}
