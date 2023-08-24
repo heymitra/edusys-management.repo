0bedefbdca744535dac99f6d3c374a1060b02ab7
mitrahe <heydarimitra98@gmail.com>
2023-08-24 15:19:15 +0000

    Update Constant (messages).

---
+++ src/main/java/org/example/util/Constant.java
@@ -0,0 +1,74 @@
+package org.example.util;
+
+public class Constant {
+
+    public static final String SAY_HI = "Welcome TO Education System Management \uD83D\uDC4B\uD83D\uDE0A";
+    public static final String SAY_BYE = "Exiting the program. Goodbye! \uD83D\uDC4B";
+    public static final String HOME_PAGE = "Home Page";
+    public static final String MENU = "MENU";
+    public static final String EMPLOYEE_EDIT = "-------- Employee Info -------";
+    public static final String STUDENT_EDIT = "-------- Student Info -------";
+    public static final String PROFESSOR_EDIT = "-------- Student Info -------";
+    public static final String COURSE_EDIT = "-------- Course Info -------";
+    public static final String LINE = "--------------------------";
+    public static final String ROLE = "Select Your Role";
+    public static final String POSITION = "Select Your Position";
+    public static final String INVALID_INPUT = "Invalid input. Please try again. ";
+    public static final String SET_USERNAME_PASSWORD = "---------- Set Username and Password ---------";
+    public static final String ASK_CHOICE = "Enter your choice: ";
+    public static final String ASK_FOR_USERNAME = "Enter username: ";
+    public static final String ANOTHER_USERNAME = "Try another username: ";
+    public static final String ASK_FOR_PASSWORD = "Enter password: ";
+    public static final String ASK_FOR_NAME = "Name: ";
+    public static final String ASK_FOR_SURNAME = "Surname: ";
+    public static final String ASK_COURSE_ID = "Course ID: ";
+    public static final String ASK_TITLE = "Course Title: ";
+    public static final String ASK_CREDIT = "Credit: ";
+    public static final String ASK_SEMESTER = "Semester: ";
+    public static final String ASK_USER_ID = "User ID: ";
+    public static final String ASK_STUDENT_ID = "Enter the student id for which you want to record grade: ";
+    public static final String INVALID_CREDIT = "Invalid credit. Credit remained unchanged... ";
+    public static final String WRONG_USERNAME = "User with this username does not exist. ";
+    public static final String WRONG_PASSWORD = "Incorrect password.\nBack to home page...";
+    public static final String WELCOME_BACK = "\n*.*.*.* Welcome Back ";
+    public static final String REGISTRATION_FAILURE = "Registration failed. ";
+    public static final String DUPLICATED_USERNAME = "Username is reserved. ";
+    public static final String LOGIN_MESSAGE = "Congratulations on your successful registration!\nTo gain access, please finalize your registration process by providing your personal information.";
+    public static final String ACCESS = "You have these accesses as a/an ";
+    public static final String SELECT_COURSE = "Select the course by entering course id. Enter \"exit\" to stop selecting.";
+    public static final String ENTER_COURSE_ID = "Course ID: ";
+    public static final String MAX_CREDITS_ALLOWED = "The maximum credit hours you are allowed to choose is: ";
+    public static final String REMAINED_CREDITS = "The remained credit hours you are allowed to choose is: ";
+    public static final String CREDIT_LIMIT = "You reached credit limit. ";
+    public static final String DUPLICATED_COURSE = "You already have taken this course. ";
+    public static final String PASSED_COURSE = "You already have passed this course in previous semesters. ";
+    public static final String COURSE_TAKEN = "Course successfully taken! ";
+    public static final String NO_COURSE_TAKEN = "You have not taken any course yet. ";
+    public static final String NO_COURSE_ASSIGNED = "You are not assigned as a professor for any courses. ";
+    public static final String COURSE_NOT_FOUND = "Course not found / Incorrect course ID";
+    public static final String USER_NOT_FOUND = "User not found / Incorrect student ID";
+    public static final String ALREADY_EVALUATED = "Student already evaluated. Cannot rerecord grade. ";
+    public static final String ASK_GRADE = "Enter Grade: ";
+    public static final String OUT_OF_RANGE = "Grade must be between 0 and 20. Please try again. ";
+    public static final String INVALID_GRADE = "Invalid input. Please enter a valid numeric grade. ";
+    public static final String ERROR_IN_TAKING_COURSE = "Error in taking course... ";
+    public static final String NO_STUDENT = "Not any student have taken this course. ";
+    public static final String NO_PROFESSOR = "There is no professor enrolled to be assigned to this course. ";
+    public static final String NUMBER_FORMAT = "Just enter number format. ";
+    public static final String OUT_OF_BOUND = "Select among options! ";
+    public static final String SALARY = "Salary is: ";
+    public static final String TO_SKIP = "(Enter - to leave this field unchanged) ";
+    public static final String NO_AVAILABLE_COURSE = "There is not any available courses yet. ";
+    public static final String DELETE_YOURSELF = "You can not delete yourself. ";
+
+
+    public static final String[] INITIAL_MENU = {"Register", "Login", "Exit"};
+    public static final String[] ROLE_MENU = {"STUDENT", "PROFESSOR", "EMPLOYEE"};
+    public static final String[] POSITION_MENU = {"FACULTY", "ADJUNCT"};
+    public static final String[] WRONG_USERNAME_MENU = {"Try again", "Register if you do not have an account"};
+    public static final String[] STUDENT_MENU = {"View Your Profile", "View Course List", "Select Courses", "View Taken Courses and Grades", "logout"};
+    public static final String[] PROFESSOR_MENU = {"View Your Profile", "Record Student Grades", "View Payroll Slip", "logout"};
+    public static final String[] EMPLOYEE_MENU = {"Access to Student Information", "Access to Professor Information", "Access to Staff Information", "Access to Course List", "View Payroll", "Exit"};
+    public static final String[] COURSE_EDIT_MENU = {"Add Course", "Delete Course", "Edit Course", "Back to Main Menu"};
+    public static final String[] PERSON_EDIT_MENU = {"Add", "Delete", "Edit", "Back to Main Menu"};
+}
